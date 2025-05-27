b566f0e981e3041af3e65961a27759fe9ceeac45
JeonYoungha <72368472+JeonYoungha@users.noreply.github.com>
2025-05-27 11:20:14 +0900

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-conn = mysql.connector.connect(
-    host='localhost',
-    user='your_user',
-    password='your_password',
-    database='your_database'
-)
-cursor = conn.cursor(dictionary=True)
